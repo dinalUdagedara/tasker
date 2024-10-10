0baaa7ce1ad19c8f6651665b08582ed53bde0645
Thiyunu <dinal.bandara@gmail.com>
2024-10-11 00:07:01 +0530

    feat: basic layout-for drawer and collapsable sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex justify-center h-full min-h-screen gap-10 mt-20 ">
-      Tasker
-      <Button>This is a Button</Button>
+      go to route : /mobile-app
     </div>
   );
 }
--- ui-components/mobile-app/mobile-app.tsx
+++ ui-components/mobile-app/mobile-app.tsx
@@ -1,15 +1,39 @@
 import MobileAppContent from "./mobile-app-content/mobile-app-content";
 import MobileAppHeader from "./mobile-app-header/mobile-app-header";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 const MobileApp = () => {
   return (
-    <div className="flex  h-full flex-col gap-10">
-      <div className="">
-        <MobileAppHeader />
-      </div>
-      <div className="">
-        <MobileAppContent />
-      </div>
+    <div className="flex h-full flex-col">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="min-h-screen h-full  w-full "
+      >
+        <ResizablePanel defaultSize={25}>
+          <div className="flex h-full items-center justify-center">
+            <span className="font-semibold">
+              <div className="">
+                <MobileAppHeader />
+              </div>
+              <div className="">
+                <MobileAppContent />
+              </div>
+            </span>
+          </div>
+        </ResizablePanel>
+        {/* Handler */}
+        <ResizableHandle withHandle />
+
+        <ResizablePanel defaultSize={0}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Content</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 };
