e2f5ce3dc2566c4592c6218384f8e84c9466e037
Thiyunu <dinal.bandara@gmail.com>
2024-10-13 20:14:43 +0530

    feat: zustand setup

--- ui-components/messages/contact-card.tsx
+++ ui-components/messages/contact-card.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import useStore from "@/store/state";
 
 interface ChatHeadProps {
   imageUrl: string;
@@ -7,22 +10,46 @@ interface ChatHeadProps {
   date: string;
 }
 
-const ContactCard: React.FC<ChatHeadProps> = ({imageUrl,name,lastText,date}) => {
+const ContactCard: React.FC<ChatHeadProps> = ({
+  imageUrl,
+  name,
+  lastText,
+  date,
+}) => {
+  const count = useStore((state) => state.count);
+  const increment = useStore((state) => state.increment);
+  const decrement = useStore((state) => state.decrement);
   return (
     <div className="flex flex-row gap-2 items-center mt-5 mb-5">
-    <div className="flex">
+
+
+      {/* Use the states like this */}
+      <div className="w-full flex justify-center items-center p-20 gap-5 flex-col">
+        <h1>{count}</h1>
+
+        <div className="flex gap-10">
+          <Button onClick={increment}>Increment</Button>
+          <Button onClick={decrement}>Decrement</Button>
+        </div>
+      </div>
+      {/* Remove these after using */}
+
+
+      
+
+      <div className="flex">
         <Avatar>
-        <AvatarImage src={imageUrl} />
-        <AvatarFallback>CN</AvatarFallback>
+          <AvatarImage src={imageUrl} />
+          <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-    </div>
-    <div className="hidden lg:flex flex-col pl-2 w-full">
+      </div>
+      <div className="hidden lg:flex flex-col pl-2 w-full">
         <div className="flex justify-between items-center">
-        <p className="text-md font-sans font-semibold">{name}</p>
-        <p className="text-xs">{date}</p>
+          <p className="text-md font-sans font-semibold">{name}</p>
+          <p className="text-xs">{date}</p>
         </div>
         <p className="text-sm font-sans font-thin">{lastText}</p>
-    </div>
+      </div>
     </div>
   );
 };
