740fca88f21cb73a84843ca6b92b2a5aa07bed65
Thiyunu <dinal.bandara@gmail.com>
2024-11-08 14:57:52 +0530

    fix: redirecting if not signed in

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,12 @@
 import Chat from "@/ui-components/messages/chat";
+import { auth } from "@/auth";
 
-const MobileAppContent = () => {
+import { redirect } from "next/navigation";
+
+const MobileAppContent = async () => {
+  const session = await auth();
+
+  if (!session?.user) redirect("/sign-in");
   return (
     <div className="container mx-auto p-4">
       <Chat />
