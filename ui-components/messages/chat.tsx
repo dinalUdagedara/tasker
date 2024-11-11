d9e04185f94915ee223671da9946b7163ff36899
Thiyunu <dinal.bandara@gmail.com>
2024-11-11 23:55:05 +0530

    feat : selecting a chatter and creating conversation with the selected user

--- ui-components/messages/chat.tsx
+++ ui-components/messages/chat.tsx
@@ -1,13 +1,39 @@
+"use client";
 import ChatHead from "@/ui-components/messages/chat-head";
 import ChatBubbleRecieve from "@/ui-components/messages/chat-bubble-recieve";
 import ChatBubbleSend from "@/ui-components/messages/chat-bubble-send";
 import { User } from "@/lib/types";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 interface ChatComponentProps {
   user: User;
 }
 
 const ChatComponent = ({ user }: ChatComponentProps) => {
+  const createConversation = useMutation(api.chat.createConversation);
+  const { data: session } = useSession(); // Get session from next-auth
+
+  useEffect(() => {
+    const initiateConversation = async () => {
+      if (user && session?.user?.email) {
+        try {
+          const conversation = await createConversation({
+            user_1_email: user.email,
+            user_2_email: session.user.email,
+          });
+          console.log("conversation", conversation); // Log the resolved conversation object
+        } catch (error) {
+          console.error("Error creating conversation", error);
+        }
+      }
+    };
+
+    initiateConversation();
+  }, [user, session]);
+
   return (
     <div>
       <div className="pr-2">
